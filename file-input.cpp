5cc6a26973e2e18cc4f4a6bcbfee7a9592555f97
Rob Hess <robwhess@gmail.com>
2018-01-17 15:24:53 -0800

    Add code from 1/17 lecture.

--- file-input.cpp
+++ file-input.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #define FILENAME "testdata.txt"
 
@@ -8,6 +9,34 @@ struct student {
 };
 
 int main(int argc, char const *argv[]) {
+
+  std::ifstream infile;
+  infile.open(FILENAME);
+  if (infile.fail()) {
+    std::cerr << "Error opening file: " << FILENAME << std::endl;
+    return 1;
+  }
+
+  // if (!infile.is_open()) {
+  //   std::cerr << "Error opening file: " << FILENAME << std::endl;
+  //   return 1;
+  // }
+
+  char c;
+  infile.get(c);
+  std::cout << "c: " << c << std::endl;
+
+  // int n;
+  // infile >> n;
+  // if (infile.fail()) {
+  //   std::cerr << "Failed to read n" << std::endl;
+  // } else {
+  //   std::cout << "n: " << n << std::endl;
+  // }
+
+
+
+  infile.close();
 
   return 0;
 }
